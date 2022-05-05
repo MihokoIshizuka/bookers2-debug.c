b7d0e4fe195e36c88960ced92ba3a6c3986e7bd3
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-05 12:31:44 +0000

    [finish]7c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,23 +1,24 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_current_user, only: [:edit,:update]
-  
+  before_action :ensure_correct_user, only: [:edit,:update]
+
   def new
     @group = Group.new
   end
-  
+
   def index
     @groups = Group.all
     @book = Book.new
+    @user = User.find(current_user.id)
   end
-  
+
   def show
     @group = Group.find(params[:id])
     @book = Book.new
   end
-  
+
   def create
-    @group = Group.new
+    @group = Group.new(group_params)
     @group.owner_id = current_user.id
     if @group.save
       redirect_to groups_path
@@ -25,5 +26,31 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  
+
+  def edit
+    
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
+
 end
